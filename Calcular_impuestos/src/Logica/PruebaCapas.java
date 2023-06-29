3e0bb5cdcb17973da733ce043b4461bff4d269c9
juan ignacio sorato <101840362+Juanisorato21@users.noreply.github.com>
2023-06-28 21:08:01 -0300

    Subo el programa

---
+++ Calcular_impuestos/src/Logica/PruebaCapas.java
@@ -0,0 +1,14 @@
+package Logica;
+
+import grafica.Pantalla;
+
+public class PruebaCapas {
+    public static void main(String[] args) {
+        
+        Pantalla panta = new Pantalla();  //Creamos el objeto panta
+        panta.setVisible(true);   //Hacemos que la pantalla sea visible
+        panta.setLocationRelativeTo(null); //Hacemos que la pantalla salga al medio de la pantalla
+        
+    }
+    
+}
